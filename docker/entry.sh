7aae2bfb5ec9c9f79a4780a2154650051ce211d8
Zalgar <corbynberndsen@gmail.com>
2023-10-22 23:58:27 +1100

    Completed working dockerfile with automatic encryption key provisioning

---
+++ docker/entry.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Check for the presence of required files
+cd /xrit-rx
+if [ -f /xrit-rx/xrit-rx.ini ] && [ -f /xrit-rx/EncryptionKeyMessage.bin ]; then
+    echo "Required files found. Starting the application..."
+    python3 xrit-rx.py
+else
+    echo "Required files not found. Please ensure xrit-rx.ini and EncryptionKeyMessage.bin are present."
+    python3 xrit-rx.py
+fi
+
