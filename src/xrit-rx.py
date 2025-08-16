68423764f1608111106b58ca72f43535491c8c6a
Zalgar <1351600+Zalgar@users.noreply.github.com>
2025-08-16 16:34:19 +1000

    update version

--- src/xrit-rx.py
+++ src/xrit-rx.py
@@ -55,7 +55,7 @@ log_max_size = None     # Log file max size in MB
 log_backup_count = None # Number of backup log files
 dashp = None            # Dashboard HTTP port
 dashi = None            # Dashboard refresh interval (sec)
-ver = "2.0.0"           # xrit-rx version
+ver = "2.0.8"           # xrit-rx version
 
 
 def start_timelapse_service():
